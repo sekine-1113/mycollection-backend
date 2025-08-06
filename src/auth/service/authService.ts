a1475a4ca23c9c7bf7647f5894a28600a131cd0a
sekine-1113 <toshiaki.nk19@gmail.com>
2025-08-06 19:10:33 +0900

    [fix] auth

--- src/auth/service/authService.ts
+++ src/auth/service/authService.ts
@@ -1,49 +1,26 @@
-import jwt, { Secret, SignOptions } from 'jsonwebtoken';
+import jwt from 'jsonwebtoken';
 import config from '../../config';
 import { HTTPException } from '../../error';
-import { UserService } from '../../users/service/userService';
-import { JWTBody } from '../../types';
-import { UserPermission } from '@prisma/client';
-
-const ACCESS_SECRET = config.ACCESS_SECRET;
-const REFRESH_SECRET = config.REFRESH_SECRET;
+import { userService, UserService } from '../../users/service/userService';
+import { UserRole } from '@prisma/client';
 
 export class AuthService {
-  private userService = new UserService();
+  private userService: UserService;
 
-  async login(email: string, password: string) {
-    const user = await this.userService.loginUser(email, password);
+  constructor(userService: UserService) {
+    this.userService = userService;
+  }
+
+  async login(email: string) {
+    const user = await this.userService.loginUser(email);
     if (!user)
       throw new HTTPException('NotFound', {
         detailMessage: 'email または password に誤りがあります。',
       });
-    const token = this.generateAccessToken(user.publicId, user.permission);
+    const token = '';
 
     return { token };
   }
-
-  generateAccessToken(publicId: string, permission: UserPermission) {
-    return jwt.sign({ publicId, permission }, ACCESS_SECRET, {
-      expiresIn: '15m',
-      algorithm: 'HS256',
-    });
-  }
-
-  generateRefreshToken(publicId: string, permission: UserPermission) {
-    return jwt.sign({ publicId, permission }, REFRESH_SECRET, {
-      expiresIn: '7d',
-      algorithm: 'HS256',
-    });
-  }
-
-  verifyRefreshToken(token: string): string {
-    try {
-      const payload = jwt.verify(token, REFRESH_SECRET) as JWTBody;
-      return payload.publicId;
-    } catch (err) {
-      throw new HTTPException('Unauthorized', {
-        detailMessage: 'リフレッシュトークンが無効です。',
-      });
-    }
-  }
 }
+
+export const authService = new AuthService(userService);
