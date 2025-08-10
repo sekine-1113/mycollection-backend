878d337be635b8d01ced7c0efb767c662db9dbc8
sekine-1113 <toshiaki.nk19@gmail.com>
2025-08-11 03:51:26 +0900

    [update] 更新

--- src/swagger/openapi-schema.ts
+++ src/swagger/openapi-schema.ts
@@ -1,3 +1,8 @@
-import userEndpoint from './endpoints/userEndpoint';
+import { generateSchema } from '../utils';
+import { usersRouterHandlers } from '../routes/users';
+import { authRouterHandlers } from '../routes/auth';
 
-export const openAPISchemas = [...userEndpoint];
+export const openAPISchemas = [
+  ...generateSchema('/auth', ['Auth'], authRouterHandlers),
+  ...generateSchema('/users', ['Users'], usersRouterHandlers),
+];
